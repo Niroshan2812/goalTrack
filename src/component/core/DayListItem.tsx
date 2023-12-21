e0063ea4a8122cf19092c9223e6e8d1d30bed750
Niroshan2812 <niroshan9812@gmail.com>
2023-12-21 23:47:25 +0530

    New Compo for Daylist item

--- src/component/core/DayListItem.tsx
+++ src/component/core/DayListItem.tsx
@@ -2,6 +2,28 @@ import{Text, View, StyleSheet} from 'react-native';
 
 export default function DayListItem () {
     return (
-<Text>Day list item</Text>
-    )
+    <View style={styles.box}>
+          <Text style={styles.text}>1</Text>
+        </View>
+    );
 }
+const styles = StyleSheet.create({
+    
+    box: {
+      backgroundColor: "#f9EDE3",
+     
+      flex :1,
+      aspectRatio:1,
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: "#9B4521",
+      borderRadius: 20,
+  
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    text: {
+      color: "#9B4521",
+      fontSize: 70,
+    },
+  });
+  
